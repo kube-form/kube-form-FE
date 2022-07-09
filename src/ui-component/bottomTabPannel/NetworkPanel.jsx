6be842bb88dad435ae760fd0a96c646ff9547a77
dwdjjj <dwdjjj@naver.com>
2022-07-10 01:52:49 +0900

    FEAT: status bottom summary, network #27

--- src/ui-component/bottomTabPannel/NetworkPanel.jsx
+++ src/ui-component/bottomTabPannel/NetworkPanel.jsx
@@ -1,11 +1,4 @@
-import {
-    Box,
-    CardContent,
-    Divider,
-    List,
-    ListItem,
-    Typography,
-} from '@mui/material';
+import { Box, CardContent, Divider, Grid, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -28,92 +21,74 @@ function NetworkPanel({ value, index }) {
             <CardContent>
                 <Typography variant="h3">Networking</Typography>
                 <Divider />
-                <List>
-                    <ListItem>
-                        <Box
-                            sx={{
-                                padding: 1,
-                                marginRight: 1,
-                                borderRight: `1px solid ${theme.palette.divider}`,
-                                width: 250,
-                            }}
-                        >
-                            <StatusListItemTitleSubTitle
-                                title="VPC"
-                                content="vpc-01a48279b85f653da"
-                            />
-                            <StatusListItemTitleSubTitle
-                                title="클러스터 IP 주소 패밀리"
-                                content="IPv4"
-                            />
-                            <StatusListItemTitleSubTitle
-                                title="서비스c IPv4 범위"
-                                content="172.20.0.0/16"
-                            />
-                        </Box>
-
-                        <Box
-                            sx={{
-                                padding: 1,
-                                marginRight: 1,
-                                borderRight: `1px solid ${theme.palette.divider}`,
-                                width: 250,
-                            }}
-                        >
-                            <StatusListItemTitleSubTitle
-                                title="서브넷"
-                                content="subnet-06939c498092bfa2d"
-                            />
-                            <StatusListItemTitleSubTitle content="subnet-02e493624d0935356" />
-                            <StatusListItemTitleSubTitle content="subnet-0ffdbbfb0361c2bcd " />
-                        </Box>
-                        <Box
-                            sx={{
-                                padding: 1,
-                                marginRight: 1,
-                                borderRight: `1px solid ${theme.palette.divider}`,
-                                width: 250,
-                            }}
-                        >
-                            <StatusListItemTitleSubTitle
-                                title="클러스터 보안 그룹"
-                                content="sg-08ad717c0205a4716"
-                            />
-                            <StatusListItemTitleSubTitle
-                                title="추가 보안 그룹"
-                                content="sg-093dbaad9dc2cf13d"
-                            />
-                        </Box>
-                        <Box
-                            sx={{
-                                pdding: 1,
-                                width: 250,
-                            }}
-                        >
-                            <StatusListItemTitleSubTitle
-                                title="API 서버 엔드포인트 액세스"
-                                content="Public"
-                            />
-                            <StatusListItemTitleSubTitle
-                                title="퍼블릭 액세스 소스 허용 목록"
-                                content="0.0.0.0/0(모든 트래픽에 개방됨)"
-                            />
-                        </Box>
-                    </ListItem>
-                </List>
-                <List>
-                    <ListItem>
-                        <StatusListItemAvatar
-                            icon={<CheckIcon color="success" />}
-                            content="running"
+                <Grid container xs={12} sx={{ flexGrow: 1 }}>
+                    <Grid
+                        item
+                        md={3}
+                        sx={{
+                            padding: 1,
+                        }}
+                    >
+                        <StatusListItemTitleSubTitle
+                            title="VPC"
+                            content="vpc-01a48279b85f653da"
                         />
-                        <StatusListItemAvatar
-                            icon={<WarningIcon color="warning" />}
-                            content="warning"
-                            sx={{ color: theme.palette.warning.dark }}
+                        <StatusListItemTitleSubTitle
+                            title="클러스터 IP 주소 패밀리"
+                            content="IPv4"
                         />
-                    </ListItem>
-                </List>
+                        <StatusListItemTitleSubTitle
+                            title="서비스c IPv4 범위"
+                            content="172.20.0.0/16"
+                        />
+                    </Grid>
+                    <Grid
+                        item
+                        md={3}
+                        sx={{
+                            padding: 1,
+                        }}
+                    >
+                        <StatusListItemTitleSubTitle
+                            title="서브넷"
+                            content="subnet-06939c498092bfa2d"
+                        />
+                        <StatusListItemTitleSubTitle content="subnet-02e493624d0935356" />
+                        <StatusListItemTitleSubTitle content="subnet-0ffdbbfb0361c2bcd " />
+                    </Grid>
+                    <Grid
+                        item
+                        md={3}
+                        sx={{
+                            padding: 1,
+                        }}
+                    >
+                        <StatusListItemTitleSubTitle
+                            title="클러스터 보안 그룹"
+                            content="sg-08ad717c0205a4716"
+                        />
+                        <StatusListItemTitleSubTitle
+                            title="추가 보안 그룹"
+                            content="sg-093dbaad9dc2cf13d"
+                        />
+                    </Grid>
+                    <Grid
+                        item
+                        md={3}
+                        sx={{
+                            padding: 1,
+                        }}
+                    >
+                        <StatusListItemTitleSubTitle
+                            title="API 서버 엔드포인트 액세스"
+                            content="Public"
+                        />
+                        <StatusListItemTitleSubTitle
+                            title="퍼블릭 액세스 소스 허용 목록"
+                            content="0.0.0.0/0(모든 트래픽에 개방됨)"
+                        />
+                    </Grid>
+                </Grid>
             </CardContent>
         </Box>
     );
