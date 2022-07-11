941addb266d6d72d6002ba1c8beb5b7a4ca6601e
ha4219 <ha4219@naver.com>
2022-07-12 00:49:18 +0900

    FIX: Overview, Network, Loging page style #30

--- src/ui-component/bottomTabComponents.jsx
+++ src/ui-component/bottomTabComponents.jsx
@@ -3,11 +3,16 @@ import {
     Typography,
     ListItemAvatar,
     Avatar,
+    IconButton,
+    Popover,
+    Box,
 } from '@mui/material';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { IconExternalLink } from '@tabler/icons';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 export function StatusListItemTitleSubTitle({ title, content, sx }) {
     const theme = useTheme();
@@ -113,16 +118,115 @@ export function StatusLinkTypography({ children, sx }) {
             sx={{
                 color: theme.palette.info.main,
                 cursor: 'pointer',
-                alignItems: 'end',
+                alignItems: 'top',
+                wordWrap: 'break-word',
+                lineHeight: 1.4,
             }}
         >
             {children}
             <IconExternalLink
-                style={{ 'vertical-align': 'bottom', marginLeft: 1 }}
+                style={{ 'vertical-align': 'top', marginLeft: 1 }}
                 width={20}
                 height={20}
             />
         </Typography>
+    );
+}
+
+export function StatusCopyTypography({ children, sx }) {
+    const theme = useTheme();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleOpen = (event) => {
+        const el = document.createElement('textarea');
+        el.value = children;
+        el.setAttribute('readonly', '');
+        el.style = { position: 'absolute', left: '-9999px' };
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+    return (
+        <Box>
+            <Popover
+                id={id}
+                anchorEl={anchorEl}
+                classes={{ paper: 'MuiPopover-paper' }}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={open}
+                onClose={handleClose}
+                sx={{ borderRadius: 0 }}
+            >
+                <Box sx={{ padding: 2, borderRadius: 0 }}>
+                    <Typography
+                        sx={{
+                            color: theme.palette.success.dark,
+                            verticalAlign: 'top',
+                        }}
+                    >
+                        <CheckCircleOutlineIcon
+                            color="success.dark"
+                            sx={{
+                                verticalAlign: 'top',
+                                width: 16,
+                                height: 16,
+                                marginRight: 0.5,
+                            }}
+                        />
+                        Copied!
+                    </Typography>
+                </Box>
+            </Popover>
+            <Typography
+                sx={{
+                    display: 'inline-block',
+                    wordWrap: 'break-word',
+                    lineHeight: 1.4,
+                }}
+                paragraph
+            >
+                <IconButton
+                    aria-describedby={id}
+                    onClick={handleOpen}
+                    sx={{
+                        padding: 0,
+                        marginLeft: 1,
+                        marginRight: 0.5,
+                        width: 16,
+                        height: 16,
+                        display: 'inline-block',
+                        float: 'left',
+                    }}
+                >
+                    <ContentCopyIcon
+                        sx={{
+                            verticalAlign: 'top',
+                            width: 16,
+                            height: 16,
+                        }}
+                    />
+                </IconButton>
+
+                {children}
+            </Typography>
+        </Box>
     );
 }
 
